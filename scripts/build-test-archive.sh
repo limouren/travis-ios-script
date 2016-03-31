d87898f7c72299704aff64e3996aa084a7725145
Kenji Pa <limouren@gmail.com>
2016-03-31 14:56:00 +0800

    Remove unnecessary build and test phrase before archive

--- scripts/build-test-archive.sh
+++ scripts/build-test-archive.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-    echo "This is a pull request. Just build in DEBUG configuration."
-    xctool -workspace $WORKSPACE_NAME.xcworkspace \
-    -scheme $SCHEME_NAME -sdk $BUILD_SDK \
-    -configuration Debug ONLY_ACTIVE_ARCH=NO
-    if [[ $? -ne 0 ]]; then
-        echo "Error: Build fail."
-        exit 1
-    fi
-
+    echo "This is a pull request. Nothing to archive."
 else
-    echo "Building..."
-    xctool -workspace $WORKSPACE_NAME.xcworkspace \
-    -scheme $SCHEME_NAME -sdk $BUILD_SDK ONLY_ACTIVE_ARCH=NO
-    if [[ $? -ne 0 ]]; then
-        echo "Error: Build fail."
-        exit 1
-    fi
-
-    echo "Running test..."
-    xctool test -workspace $WORKSPACE_NAME.xcworkspace \
-    -scheme $SCHEME_NAME -sdk $BUILD_SDK ONLY_ACTIVE_ARCH=NO
-    if [[ $? -ne 0 ]]; then
-        echo "Error: Test fail."
-        exit 1
-    fi
-
     echo "Making archive..."
     xctool -workspace $WORKSPACE_NAME.xcworkspace -scheme $SCHEME_NAME \
     -sdk $RELEASE_BUILD_SDK -configuration Release ONLY_ACTIVE_ARCH=NO \
